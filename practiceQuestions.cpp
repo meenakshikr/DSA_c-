cdb2fa3e293220a1c72ad4cef4706ffefa71e9bd
meenakshikr <136142896+meenakshikr@users.noreply.github.com>
2024-07-11 08:02:36 +0530

    Add files via upload

---
+++ practiceQuestions.cpp
@@ -0,0 +1,59 @@
+ #include<iostream>
+ #include<math.h>
+ using namespace std;
+
+ int main(){
+    // whether PRIME OR NOT
+   /* int n;
+    cin>> n;
+    bool flag = 0;
+
+    for(int i = 2; i<n; i++){
+        if(n%i == 0){
+            cout<<"No is not prime"<<endl;
+            flag = 1;
+            break;
+           
+        }
+    }
+    if(flag ==0){
+        cout<<"no is prime"<<endl;
+    }
+    */
+                   // REVERSE A NUMBER
+    
+    /*int n;
+    cin>>n;
+
+    int reverse =0;
+    while(n>0){
+        int last_digit = n%10;
+        reverse = reverse*10 + last_digit;
+        n = n/10;
+    }
+
+    cout<<reverse<<endl;
+    */
+                /* ARMSTRONG NUMBER
+   int n;
+   cin>>n;
+    int sum = 0;
+    int originalNumber = n;
+   while(n>0){
+    int last_digit = n%10;
+    sum += pow(last_digit,3);
+    n=n/10;
+
+   }
+   if(originalNumber== sum){
+    cout<<"an armstrong no"<<endl;
+   }
+   else{
+    cout<<"not armstrong no"<<endl;
+   }
+   return 0;
+
+   */
+
+  
+ }
